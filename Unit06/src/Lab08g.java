27beb22cd0d3ee655ef21cbf916473c1a98c92dd
AndrewSchade <SchadeA7328@sduhsd.lan>
2018-02-20 11:46:50 -0800

    Unit 6

---
+++ Unit06/src/Lab08g.java
@@ -0,0 +1,24 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class Lab08g
+{
+	public static void main ( String[] args )
+	{
+		//add test cases
+		int num1 = 1;
+		int num2 = 5;
+		LoopStats MyStats = new LoopStats(num1, num2);
+		out.println(MyStats.toString());
+		
+		
+		
+		
+					
+	}
+}
---
+++ Unit06/src/Lab08i.java
@@ -0,0 +1,24 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+import java.lang.Math;
+
+public class Lab08i
+{
+	public static void main ( String[] args )
+	{
+		int num1 = 24;
+		int num2 = 7;
+		Prime MyPrime = new Prime(num1);
+		out.println(MyPrime.toString());
+		
+		
+		
+		
+		
+	}	
+}
