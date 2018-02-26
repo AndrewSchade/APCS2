507aeb8ac73a2b3110c4de8655024a6a9537acf1
AndrewSchade <SchadeA7328@sduhsd.lan>
2018-02-26 11:34:44 -0800

    Unit 7 & 8

--- Unit07/src/Lab11c.java
+++ Unit07/src/Lab11c.java
@@ -10,7 +10,9 @@ public class Lab11c
 {
    public static void main( String args[] )
    {
-
+	   TriangleThree MyThree = new TriangleThree(3 , "A");
+	   out.println(MyThree.toString());
+	   
 
 
 
--- Unit07/src/TriangleThree.java
+++ Unit07/src/TriangleThree.java
@@ -13,24 +13,45 @@ public class TriangleThree
 
 	public TriangleThree()
 	{
+		int size = 0;
+		String letter = "";
 	}
 
 	public TriangleThree(int count, String let)
 	{
+		setTriangle(let, count);
 	}
 
 	public void setTriangle( String let, int sz )
 	{
+		letter = let;
+		size = sz;
 	}
 
 	public String getLetter()
 	{
-		return "#";
+		return letter;
 	}
 
 	public String toString()
 	{
 		String output="";
+		int sz = size;
+		int sze = 1;
+		for(int i = 0; i < size; i++)
+		{
+			for(int j = sz; j > 1; j--)
+			{
+				output = output + " ";
+			}
+			for(int h = 0; h < sze; h++)
+			{
+				output = output + getLetter();
+			}
+			output = output + "\n"; 
+			sz = sz - 1;
+			sze = sze + 1;
+		}
 		return output+"\n";
 	}
 }
